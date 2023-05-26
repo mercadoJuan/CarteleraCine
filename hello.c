24dc607656dc143d61b26b074e33a2cc062d00f5
JuanMercado <mercadojuanc25@gmail.com>
2023-05-26 15:26:16 -0300

    Estoy seguro de este commit n1

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a=0;
+
+    printf("hello world!\n");
+    printf("ingrese un valor: ");
+    scanf("%d",&a);
+
+    printf("\n");
+    printf("el valor de la variable a es: %d",a);
+    return 0;
+    
+}
