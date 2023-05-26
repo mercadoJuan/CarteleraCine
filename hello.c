b77835480bf724abc6534cb690a5cdefa90c6891
JuanMercado <mercadojuanc25@gmail.com>
2023-05-26 16:00:45 -0300

    Ahora el programa resuelve la suma de 2 valores

--- hello.c
+++ hello.c
@@ -2,13 +2,17 @@
 
 int main(){
     int a=0;
+    int b=0;
 
     printf("hello world!\n");
-    printf("ingrese un valor: ");
+    printf("ingrese un valor para A: ");
     scanf("%d",&a);
+    printf("\n");
+    printf("ingrese un valor para B: ");
+    scanf("%d",&b);
 
     printf("\n");
-    printf("el valor de la variable a es: %d",a);
+    printf(" *** LA SUMA DE A + B ES: %d + %d = %d",a,b, a+b);
     return 0;
     
 }
